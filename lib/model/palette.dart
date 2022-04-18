793986186da5f45d85200c4ad55b778f490afbe4
Da hee <ekdudekgml35@naver.com>
2022-04-18 09:55:02 +0900

    이후에 색상을 편하게 업데이트 하기 위해서, 팔레트 색상 정함

---
+++ lib/model/palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color iconColor = Color(0xFFB6C7D1);
+  static const Color activeColor = Color(0xFF09126C);
+  static const Color textColor1 = Colors.black26;
+  static const Color textColor2 = Colors.black45;
+  static const Color googleColor = Color(0xFFDE4B39);
+  static const Color backgroundColor = Colors.white30;
+  static const Color github = Colors.black;
+}
