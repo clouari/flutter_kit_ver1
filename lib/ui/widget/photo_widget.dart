09b4c16378d74660f0e96cc20fdb1edb593c5c39
Da hee <ekdudekgml35@naver.com>
2022-04-19 15:54:48 +0900

    photo 보여주기

---
+++ lib/ui/widget/photo_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_kit_ver1/model/photo.dart';
+
+class PhotoWidget extends StatelessWidget {
+  final Photo photo;
+
+  const PhotoWidget({
+    Key? key,
+    required this.photo,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(16),
+        ),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(photo.previewURL),
+        ),
+      ),
+    );
+  }
+}
