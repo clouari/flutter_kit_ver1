0020ebba3f86315aac3c2c26e91ab52233fee969
Da hee <ekdudekgml35@naver.com>
2022-04-20 16:57:47 +0900

    api 주소 중에서 수정 될 필요가 없는 코드는 final 로 정의하고 간단하게 사용할 수 있게 변경

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -4,9 +4,13 @@ import 'package:flutter_kit_ver1/model/photo.dart';
 import 'package:http/http.dart' as http;
 
 class PixabayApi {
+  final baseUrl =
+      'https://pixabay.com/api/'; // 수정 될 필요가 없는 코드이기 때문에 final, const 로 할 거면 +static
+  final key = '24806100-af9f83f3fdbb69c73da3e93f1';
+
   Future<List<Photo>> fetch(String query) async {
-    final response = await http.get(Uri.parse(
-        'https://pixabay.com/api/?key=24806100-af9f83f3fdbb69c73da3e93f1&q=$query&image_type=photo&pretty=true'));
+    final response = await http.get(
+        Uri.parse('$baseUrl?key=$key&q=$query&image_type=photo&pretty=true'));
 
     Map<String, dynamic> jsonResponse = jsonDecode(response.body);
     // map 형태로 얻은 데이터는 Json 형태처럼 사용이 가능 해 진다.
