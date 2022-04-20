2318319bf0cac6882faebe43ad7b67853ad4d3cc
Da hee <ekdudekgml35@naver.com>
2022-04-20 16:49:33 +0900

    data 로직만 처리하는 폴더 안에 UI 에 같이 있던 기능 코드 분리 -> 추가로 분리 할 예정임

---
+++ lib/data/api.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:flutter_kit_ver1/model/photo.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<Photo>> fetch(String query) async {
+  final response = await http.get(Uri.parse(
+      'https://pixabay.com/api/?key=24806100-af9f83f3fdbb69c73da3e93f1&q=$query&image_type=photo&pretty=true'));
+
+  Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+  // map 형태로 얻은 데이터는 Json 형태처럼 사용이 가능 해 진다.
+
+  Iterable hits = jsonResponse['hits'];
+  return hits.map((e) => Photo.fromJson(e)).toList();
+} //여기까지는 기능단계
